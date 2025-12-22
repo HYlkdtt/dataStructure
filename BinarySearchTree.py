00eb226ccceb7d1f0dc25efa9e1c417e17a350e8
haoyang <h33luo@uwaterloo.ca>
2025-12-22 03:44:25 -0500

    Finished BinarySearchTree Implementation

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self, val):
         self.val = val
         self.left = None
-        self.Right = None
+        self.right = None
 
 class BinarySearchTree:
     def __init__(self):
@@ -14,6 +14,35 @@ class BinarySearchTree:
             return None
 
         ptr = self.root
-        while self.root:
+        while ptr:
             if val < ptr.val:
+                if not ptr.left:
+                    ptr.left = Node(val)
+                    return None
                 ptr = ptr.left
+
+            elif val > ptr.val:
+                if not ptr.right:
+                    ptr.right = Node(val)
+                    return None
+                ptr = ptr.right
+            
+            else:
+                return None
+        
+        return None
+        
+    
+    def contains(self, val):
+        ptr = self.root
+        while ptr:
+            if val == ptr.val:
+                return True
+
+            elif val < ptr.val:
+                ptr = ptr.left
+            
+            else:
+                ptr = ptr.right
+
+        return False
